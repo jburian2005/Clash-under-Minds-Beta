58d4d5d8a82ab95162a5d2ec1cf3d6d38776d051
Justin <s4005560@edu.dhsn.de>
2026-01-16 11:32:15 +0100

    beta test

--- main.py
+++ main.py
@@ -3021,3 +3021,8 @@ if __name__ == '__main__':
         socketio.run(app, host='0.0.0.0', port=port)
     else:
         socketio.run(app, host='0.0.0.0', port=port, debug=True)
+
+
+
+
+        # blabla
